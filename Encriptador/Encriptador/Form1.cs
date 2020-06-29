d7c8bd3cb9efeb936ab344a513bea98f10d05e34
MIKEKINGDOOM <63214450+MIKEKINGDOOM@users.noreply.github.com>
2020-06-28 20:11:11 -0500

    encriptador basico

---
+++ Encriptador/Encriptador/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encriptador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string encriptar;
+        
+        public string mensaje()
+        {
+            string frase = txtmensaje.Text;
+            char[] arg = frase.ToCharArray();
+            for( int i=0; i< frase.Length; i = i +1)
+            {
+                arg[i] = (char)(arg[i] + (char)11);
+                encriptar += arg[i];
+            }
+            return encriptar;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mensaje();
+            txtencriptado.Text = encriptar;
+        }
+    }
+}
